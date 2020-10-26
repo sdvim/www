67f208961cb3d3a870180a3a2f100cceb273bb49
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-10-26 15:14:05 -0600

    Add strapi backend

---
+++ strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ae0b314c032d589edb197b1f26a4287f'),
+    },
+  },
+});
---
+++ strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '509da8e8-8557-4b0c-9756-5c9d4f16cd93'
+};
