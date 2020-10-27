ece49be975aecddc9e29c6831bf9acf5d4d61ac0
Steve Della Valentina <s.dellavalentina@gmail.com>
2020-10-27 10:05:13 -0600

    Add layout structure to frontend

--- eleventy/.eleventy.js
+++ eleventy/.eleventy.js
@@ -2,4 +2,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addNunjucksFilter("where", function(arr, key, value) {
     return arr.filter(item => item[key] == value);
   });
+  return {
+    dir: {
+      includes: "_includes",
+      layouts: "_layouts",
+    }
+  }
 };
