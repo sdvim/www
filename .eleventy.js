2aa102c488c5f8341adf801a39500221d759f188
Steve <s.dellavalentina@gmail.com>
2021-06-15 22:58:42 -0600

    fix display of dates

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
+const { DateTime } = require("luxon");
 const pluginNavigation = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
-  eleventyConfig.addFilter("stringDate", string => {
-    return new Date(string).toLocaleDateString();
+  eleventyConfig.addFilter("stringDate", dateObj => {
+    return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL dd, yyyy");
   });
   eleventyConfig.addPassthroughCopy({ "./src/_assets": "assets" });
   eleventyConfig.addPlugin(pluginNavigation);
