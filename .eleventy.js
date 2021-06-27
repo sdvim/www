4dae2cda0be0ecbf7dc578d43dc1828e0d97db34
Steve <s.dellavalentina@gmail.com>
2021-06-27 17:54:36 -0600

    fix dump filter for collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,13 @@
 const { DateTime } = require("luxon");
 const pluginNavigation = require("@11ty/eleventy-navigation");
+const util = require("util");
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("stringDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("LLLL dd, yyyy");
+  });
+  eleventyConfig.addFilter("dump", obj => {
+    return util.inspect(obj);
   });
   eleventyConfig.addPassthroughCopy("./src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("./src/*.png");
