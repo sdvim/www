2785286880380e942949d708070c12ff88204b8a
Steve Della Valentina <s.dellavalentina@gmail.com>
2022-07-23 15:33:18 -0600

    Update build config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,9 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sdv.im',
+  build: {
+    format: 'file'
+  },
   integrations: [sitemap()]
 });
