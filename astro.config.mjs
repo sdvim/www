44d75447c5fd33c576c73800fb00915954bb162b
Steve Della Valentina <stevedev@google.com>
2024-07-09 09:49:21 -0600

    Update Astro; switch to pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,4 @@
 import { defineConfig } from 'astro/config';
 
-import sitemap from "@astrojs/sitemap";
-
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://sdv.im',
-  build: {
-    format: 'file'
-  },
-  integrations: [sitemap()]
-});
+export default defineConfig({});
